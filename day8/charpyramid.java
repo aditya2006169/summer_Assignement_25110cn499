361a4265cd11a2e2ccdb6bba9ee4b72a71308389
Aditya Mishra <adityaamishra51@gmail.com>
2026-07-04 01:36:01 +0530

    day8 uploaded

---
+++ day8/charpyramid.java
@@ -0,0 +1,11 @@
+public class charpyramid {
+    public static void main(String[] args) {
+        for(char i='A';i<'F';i++){
+            for(char j='A';j<=i;j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println("");
+        }
+    }
+}
+
